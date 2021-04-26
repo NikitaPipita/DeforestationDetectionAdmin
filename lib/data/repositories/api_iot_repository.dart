ac25f60a4d27cb6621c8335632739427a713fc6a
Nikita Shuliak <artur.rejet.vseh@gmail.com>
2021-04-27 01:03:00 +0300

    Implemented api iot repository.

--- lib/data/repositories/api_iot_repository.dart
+++ lib/data/repositories/api_iot_repository.dart
@@ -1,44 +1,40 @@
 import 'package:deforestation_detection_admin/converters/factory.dart';
+import 'package:deforestation_detection_admin/data/gateways/api_iot_gateway.dart';
 import 'package:deforestation_detection_admin/data/models/iot_dto.dart';
 import 'package:deforestation_detection_admin/domain/entities/iot.dart';
 import 'package:deforestation_detection_admin/domain/repositories/iot_repository.dart';
 
 class ApiIotRepository implements IotRepository {
+  final ApiIotGateWay _apiIotGateWay;
   final Factory<Iot, IotDto> _iotFromDtoFactory;
   final Factory<IotDto, Iot> _iotToDtoFactory;
 
   ApiIotRepository(
+    this._apiIotGateWay,
     this._iotFromDtoFactory,
     this._iotToDtoFactory,
   );
 
   @override
-  Future<List<Iot>> getIots() {
-    // TODO: implement getIots
-    throw UnimplementedError();
+  Future<List<Iot>> getIots() async {
+    final List<IotDto> iotsDto = await _apiIotGateWay.getIots();
+    return iotsDto.map((IotDto e) => _iotFromDtoFactory.create(e)).toList();
   }
 
   @override
-  Future<Iot> getIot(int id) {
-    // TODO: implement getIot
-    throw UnimplementedError();
+  Future<Iot> getIot(int id) async {
+    final IotDto iotDto = await _apiIotGateWay.getIot(id);
+    return _iotFromDtoFactory.create(iotDto);
   }
 
   @override
-  Future<void> createIot(Iot iot) {
-    // TODO: implement createIot
-    throw UnimplementedError();
-  }
+  Future<void> createIot(Iot iot) =>
+      _apiIotGateWay.createIot(_iotToDtoFactory.create(iot));
 
   @override
-  Future<void> updateIot(Iot iot) {
-    // TODO: implement updateIot
-    throw UnimplementedError();
-  }
+  Future<void> updateIot(Iot iot) =>
+      _apiIotGateWay.updateIot(_iotToDtoFactory.create(iot));
 
   @override
-  Future<void> deleteIot(int id) {
-    // TODO: implement deleteIot
-    throw UnimplementedError();
-  }
+  Future<void> deleteIot(int id) => _apiIotGateWay.deleteIot(id);
 }

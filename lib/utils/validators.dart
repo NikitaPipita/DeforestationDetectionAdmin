b2d6363ab703be16a966be92132a0a684ccb7569
Nikita Shuliak <artur.rejet.vseh@gmail.com>
2021-05-01 18:45:45 +0300

    Changed api base url.

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,7 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:email_validator/email_validator.dart';
 
 String? emailValidator(String? value) {
-  const String emailNotValidMessage = 'Email not valid';
+  final String emailNotValidMessage = 'email_not_valid'.tr();
   if (value == null || !EmailValidator.validate(value)) {
     return emailNotValidMessage;
   }
